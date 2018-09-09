8bc0f89a3c2eb55767a0f35758d78a1706624f9f
link00000000 <crandall.logan@gmail.com>
2018-09-08 21:25:12 -0400

    Setup ejs rendering and routing to / and /upload

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 // Dependencies
-const express = require("express");
-const dotenv = require("dotenv");
+const express = require('express');
+const dotenv = require('dotenv');
+const ejs = require('ejs');
+
+const app = express();
 
 // Initialization
 dotenv.config();
-const app = express();
+
+app.use('/public', express.static('public'))
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 // Global consts
 const PORT = process.env.PORT || 8080;
 
-
 // Port listen
-app.listen(PORT, function(){
-        console.log("Listening on port " + PORT + " :D")
+app.listen(PORT, () => {
+        console.log('Listening on port ' + PORT + ' :D')
+});
+
+// Routes
+app.get('/', (req, res) => {
+        res.render('home');
+});
+
+app.get('/upload', (req, res) => {
+        res.render('upload');
 });
