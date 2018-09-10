e4f24ac2226cfc40444e66b84e2959a7271aac67
rhohio <kad145@zips.uakron.edu>
2018-09-09 20:40:23 -0400

    General cleanup/formatting

--- src/index.js
+++ src/index.js
@@ -8,11 +8,8 @@ const mongojs = require('mongojs');
 
 // Initialization
 const app = express();
-
 let db = mongojs('fldrppr', ['users']);
-
 dotenv.config();
-
 app.use('/public', express.static('public'))
 app.use('/public/dist', express.static('bower_components'));
 app.set('view engine', 'ejs');
@@ -21,11 +18,6 @@ app.set('views', 'views');
 // Global variables
 const PORT = process.env.PORT || 8080;
 const config = require(path.join(__dirname, '..', 'config.json'));
-
-let memStorage = multer.memoryStorage();
-let upload = multer({
-    storage: memStorage
-});
 
 // Port listen
 app.listen(PORT, () => {
@@ -47,12 +39,3 @@ app.post('/file-upload', upload.single('file'), (req, res) => {
     console.log(req.file);
     return res.status(200).send('Success');
 });
-
-// Database Checking Debug
-app.get('/monGOD', (req, res) => {
-    db.users.find(function (err, docs) {
-        if (err) throw err;
-        console.log(docs);
-        res.send(docs[0].email);
-    })
-})
