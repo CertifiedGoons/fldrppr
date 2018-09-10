3a709d4f58a4b3e7809c4e2e34d16b8a390519a7
rhohio <kad145@zips.uakron.edu>
2018-09-09 23:43:31 -0400

    Minor error checking (iactually wrote something so fuck off logan)

--- src/index.js
+++ src/index.js
@@ -8,16 +8,22 @@ const mongojs = require('mongojs');
 const mongoose = require('mongoose');
 const gridFsStorage = require('multer-gridfs-storage');
 const expressValidator = require('express-validator');
+const bodyParser = require('body-parser');
 
 /*
- * Initialization
+ * Start Initialization
  */
+ 
  // Express
 const app = express();
+const check = expressValidator.check;
+const validationResult = expressValidator.validationResult;
 app.use('/public', express.static('public'))
 app.use('/public/dist', express.static('bower_components'));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 // MongoDB
 const mongoConnection = mongoose.connect('mongodb://localhost:27017/fldrppr');
@@ -39,6 +45,11 @@ app.listen(PORT, () => {
     console.log('Listening on port ' + PORT + ' :D')
 });
 
+/*
+ * End Initialization
+ */
+
+
 // Routes - get
 app.get('/', (req, res) => {
     return res.render('home');
@@ -56,12 +67,24 @@ app.get('/signup', (req, res) => {
 
 // Sign up Page back-end
 app.post('/signup', (req, res) => {
+    req.checkBody('username', 'Username is Required').notEmpty();
+    req.checkBody('email', 'Email is Required').notEmpty();
+    req.checkBody('password', 'Password is Required').notEmpty();
 
-    let newUser = {
-        userName: req.body.userName,
-        passwd: req.body.passwd,
-        email: req.body.email
+    let errors = req.validationErrors();
+
+    if(errors){
+
+    } else {
+        let newUser = {
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        }
     }
+
+    
+    res.status(200).send();
 });
 // Upload page back-end
 app.post('/file-upload', upload.single('file'), (req, res) => {
