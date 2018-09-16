1f2c9a5748b613b055c362895e2cd8e1c3ed1d01
Logan <crandall.logan@gmail.com>
2018-09-16 19:44:40 -0400

    converted css to sass

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.listen(process.env.PORT || 8080, () => {
 });
 
 // database connection
-const db = mongoose.connect(process.env.MONGO || 'mongodb://localhost:27017/fldrppr');
+const db = mongoose.connect(process.env.MONGO || 'mongodb://localhost:27017/fldrppr', { useNewUrlParser: true });
 const storage = new gridFsStorage({
     db: db,
     file: () => { return { bucketName: 'uploaded' } }
@@ -34,7 +34,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, '..', 'public')));
 app.use('/public/dist', express.static('bower_components'));
 
 // Load routes via router file
